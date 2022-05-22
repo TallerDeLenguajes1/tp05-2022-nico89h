7d410d8a47bb25cf11ee0ece559e18ff743e9649
Nicolas Leal <nicoleal14h@gmail.com>
2022-05-22 11:06:23 -0300

    otro commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 //inicio de la prueba de los strings
+Console.WriteLine("Dame una cadena a ingresar");
+string usuario=Console.ReadLine();
+int i=0;
+foreach (var item in usuario)
+{
+    Console.WriteLine("Letra: "+item);
+    i++;
+}
+Console.WriteLine("La longitud de la cadena es: "+i);
+Console.WriteLine("Dame otra cadena a buscar");
+string Cadena=Console.ReadLine();
+string Aux=usuario+Cadena;
+Console.WriteLine("La concatenacion de las dos cadenas es: "+Aux);
+Console.WriteLine("Sub cadena de la primera cadena ingresada, comienza de desde el 2do caracter: "+ usuario.Substring(2));
---
+++ readme.md
@@ -0,0 +1,17 @@
+Ejercicio 4
+1)String es un tipo de referencia
+2)Tiene muchas secuencias de escape, las mas conocidas o utilizadas son:
+\ '- comilla simple (0x0027)
+\ "- comillas dobles (0x0022)
+\\ - barra invertida (0x005C)
+\ 0 - nulo (0x0000)
+\ a - alerta (0x0007)
+\ b - retroceso (0x0008)
+\ f - alimentación de formulario (0x000C)
+\ n - nueva línea (0x000A)
+\ r - retorno de carro (0x000D)
+\ t - pestaña horizontal (0x0009)
+\ v - pestaña vertical (0x000B)
+\ u0000 - \ uFFFF - Carácter Unicode
+\ x0 - \ xFFFF - Carácter Unicode (código con longitud variable)
+\ U00000000 - \ U0010FFFF - Carácter Unicode (para generar sustitutos)
